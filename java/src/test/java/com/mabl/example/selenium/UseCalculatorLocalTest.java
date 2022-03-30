24f7213026c95c810b0fc1ed1a67c4130da89953
James Baldassari <james@mabl.com>
2022-03-30 11:51:31 -0400

    Add local version of the calculator test that does not use a proxy (#7)

--- java/src/test/java/com/mabl/example/selenium/UseCalculatorLocalTest.java
+++ java/src/test/java/com/mabl/example/selenium/UseCalculatorLocalTest.java
@@ -3,11 +3,17 @@ package com.mabl.example.selenium;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class UseCalculatorTest extends SeleniumTestWithProxySupport {
+public class UseCalculatorLocalTest extends SeleniumTestWithProxySupport {
+
+    public UseCalculatorLocalTest() {
+        super(false);
+    }
+
     @Test
     public void useCalculator() throws Exception {
         driver.get("http://juliemr.github.io/protractor-demo/");
@@ -16,7 +22,7 @@ public class UseCalculatorTest extends SeleniumTestWithProxySupport {
         driver.findElement(By.cssSelector("[ng-model=first]")).sendKeys("1");
         driver.findElement(By.cssSelector("[ng-model=second]")).click();
         driver.findElement(By.cssSelector("[ng-model=second]")).sendKeys("2");
-        driver.findElement(By.id("gobutton")).click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", driver.findElement(By.id("gobutton")));
         Thread.sleep(3000L);
         assertThat(driver.findElement(By.cssSelector(".ng-binding:nth-child(5)")).getText(), is("3"));
     }
